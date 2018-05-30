dffe8fcd4a868bd08ffd942c8469d7142001f715
Bradyn Corkill <Bradyn.c123@hotmail.com>
2018-05-30 23:46:20 +1000

    update on the Vector 4

--- MathApp/MathLib/Vector4.cpp
+++ MathApp/MathLib/Vector4.cpp
@@ -1,4 +1,5 @@
 #include "Vector4.h"
+#include <math.h>
 
 
 
@@ -12,77 +13,93 @@ Vector4::Vector4(float m_x, float m_y, float m_z, float m_w)
 
 Vector4 Vector4::operator+(const Vector4 & other)
 {
-	return Vector4();
+	return Vector4(m_x + other.m_x, m_y + other.m_y, m_z + other.m_z, m_w + other.m_w);
 }
 
 Vector4 Vector4::operator-(const Vector4 & other)
 {
-	return Vector4();
+	return Vector4(m_x - other.m_x, m_y - other.m_y, m_z - other.m_z, m_w - other.m_w);
 }
 
 Vector4 Vector4::operator*(float other)
 {
-	return Vector4();
+	return Vector4(m_x * other, m_y * other, m_z * other, m_w * other);
 }
 
 Vector4 Vector4::operator=(const Vector4 & other)
 {
-	return Vector4();
+	return Vector4(m_x = other.m_x, m_y = other.m_y, m_z = other.m_z, m_w = other.m_w);
 }
 
 float Vector4::dot(const Vector4 & other) const
 {
-	return 0.0f;
+	return m_x * other.m_x + m_y * other.m_y + m_z * other.m_z + m_w * other.m_w;
+}
+
+Vector4 operator*(const float & scalar, const Vector4 & a_vec)
+{
+	return Vector4(scalar * a_vec.m_x, scalar * a_vec.m_y, scalar * a_vec.m_z, scalar * a_vec.m_w);
 }
 
 float Vector4::operator[](int index) const
 {
-	return 0.0f;
+	return data[index];
 }
 
-Vector4::operator float*()
-{
-}
+Vector4::operator float*() { return &m_x; }
 
-Vector4::operator const float*() const
-{
-}
+
+Vector4::operator const float*() const { return &m_x; }
+
 
 float Vector4::magnitude() const
 {
-	return 0.0f;
+	return sqrt(m_x * m_x + m_y * m_y + m_z * m_z + m_w * m_w);
 }
 
 float Vector4::distance(const Vector4 & other) const
 {
-	return 0.0f;
+	float diffX = m_x - other.m_x;
+	float diffY = m_y - other.m_y;
+	float diffZ = m_z - other.m_z;
+	float diffW = m_w - other.m_w;
+	return sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ + diffW * diffW);
 }
 
 void Vector4::normalise()
 {
+	float mag = sqrt(m_x * m_x + m_y * m_y + m_z * m_z + m_w * m_w);
+	m_x;
+	m_y;
+	m_z;
+	m_w;
 }
 
 Vector4 Vector4::normalised() const
 {
-	return Vector4();
+	float mag = sqrt(m_x * m_x + m_y * m_y + m_z * m_z + m_w * m_w);
+	return { m_x / mag, m_y / mag, m_z / mag, m_w / mag };
 }
 
 Vector4 Vector4::cross(const Vector4 & other) const
 {
-	return Vector4();
+	return { m_y * other.m_z - m_z * other.m_y,
+			m_z * other.m_x - m_x * other.m_z,
+			m_x * other.m_y - m_y * other.m_x,
+			m_w};
 }
 
 float Vector4::angleBetween(const Vector4 & other) const
 {
-	return 0.0f;
+	Vector4 a = normalised();
+	Vector4 b = other.normalised();
+
+	float d = a.m_x * b.m_x + a.m_y * b.m_y + a.m_z * b.m_z;
+
+	return acos(d);
 }
 
 
 Vector4::~Vector4()
 {
-}
-
-Vector4 operator*(const float & scalar, const Vector4 & a_vec)
-{
-	return Vector4();
 }
